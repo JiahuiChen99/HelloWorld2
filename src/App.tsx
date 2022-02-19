d2410aa47d45155e4ebaf2e597de799049a9fd2f
JiahuiChen99 <Jiahui1@hotmail.es>
2022-02-19 19:24:54 +0100

    #7 feat: add navbar to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import HomePage from "./Pages/HomePage";
+import NavBar from "./Components/NavigationBar/NavBar";
 
 function App() {
     return (
-        <div className="flex w-screen h-screen overflow-x-hidden">
+        <div className="flex flex-col w-screen h-screen overflow-x-hidden">
+            <NavBar/>
             <HomePage/>
         </div>
     );
