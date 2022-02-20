c919f1526daec3a8737eb04dae1cafc1e0b334cc
JiahuiChen99 <Jiahui1@hotmail.es>
2022-02-20 22:07:21 +0000

    #9 feat: featured projects page

---
+++ src/Components/HomePage/FeaturedProjects.tsx
@@ -0,0 +1,9 @@
+const FeaturedProjects = () => {
+    return(
+        <section id="Featured Projects" className="flex w-full h-auto py-20 px-10 md:px-20 lg:px-32 xl:40 2xl:px-96">
+            <h1 className="text-2xl font-bold"> Featured Projects </h1>
+        </section>
+    )
+}
+
+export default FeaturedProjects;
