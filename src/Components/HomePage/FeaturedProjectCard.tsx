69b45fb9a198741d9ffbdb495a985f4383149141
JiahuiChen99 <Jiahui1@hotmail.es>
2022-02-22 18:31:38 +0000

    #10 feat: featured project's description card layout

--- src/Components/HomePage/FeaturedProjectCard.tsx
+++ src/Components/HomePage/FeaturedProjectCard.tsx
@@ -33,36 +33,68 @@ const FeaturedProjectCard = (props: {id: number, project: FeaturedProject}) => {
     // Callback function for show docs button & close docs button
     const ShowDocs = () => setShowDocs( show => !show );
 
+    // Display featured project title or project information
+    const CardInformation = () => {
+        if ( showDocs ) {
+            return (
+                <div className="flex w-1/2 h-full p-20 ">
+                    <div className="flex flex-col w-full h-full z-10 place-items-center gap-y-5 overflow-y-auto">
+                        {/* Project description */}
+                        <div className="flex w-full h-auto text-justify">
+                            { props.project.description }
+                        </div>
+
+                        {/* Back button */}
+                        <button className="flex w-fit h-auto hover:cursor-pointer p-2 px-5 rounded-xl bg-blue-500
+                                           shadow-lg shadow-blue-500/50 font-bold"
+                                onClick={ () => { ShowDocs() } }
+                        >
+                            Back
+                        </button>
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                <div className={`flex flex-col w-1/2 place-items-center place-content-center gap-y-3 z-10 
+                            ${props.id % 2 === 0 ? '' : 'order-last'}`}
+                >
+                    {/* Logo & Short title */}
+                    <div className="flex w-auto place-items-center font-bold text-2xl gap-x-2">
+                        { LogoShortTitle() }
+                    </div>
+
+                    {/* Title */}
+                    <span className="text-5xl text-center font-bold px-20"> { props.project.name } </span>
+
+                    {/* Icon Buttons section */}
+                    {
+                        cardHover && (
+                            <div className="flex w-auto h-auto space-x-5">
+                                {
+                                    props.project.icons.map( (icon, index) => {
+                                        return <IconButton key={index} id={index} url={icon.link} icon={icon.icon} name={icon.name}
+                                                           onOpenDocs={ ShowDocs }
+                                        />
+                                    })
+                                }
+                            </div>
+                        )
+                    }
+                </div>
+            )
+        }
+    }
+
     return(
         <div className={`flex relative w-full h-[50vh] rounded-xl bg-gradient-to-br ${props.project.colors.join(' ')}`}
              onMouseEnter={ () => {setHover(true)} }
              onMouseLeave={ () => {setHover(false)} }
         >
             <CardParticles id={props.id}/>
-            <div className={`flex flex-col w-1/2 place-items-center place-content-center gap-y-3 z-10 
-                            ${props.id % 2 === 0 ? '' : 'order-last'}`
-            }>
-                {/* Logo & Short title */}
-                <div className="flex w-auto place-items-center font-bold text-2xl gap-x-2">
-                    { LogoShortTitle() }
-                </div>
 
-                {/* Title */}
-                <span className="text-5xl text-center font-bold px-20"> { props.project.name } </span>
-
-                {/* Icon Buttons section */}
-                {
-                    cardHover && (
-                        <div className="flex w-auto h-auto space-x-5">
-                            {
-                                props.project.icons.map( (icon, index) => {
-                                    return <IconButton key={index} id={index} url={icon.link} icon={icon.icon} name={icon.name}/>
-                                })
-                            }
-                        </div>
-                    )
-                }
-            </div>
+            {/* Project title & project information */}
+            { CardInformation() }
 
             {/* Image section*/}
         </div>
